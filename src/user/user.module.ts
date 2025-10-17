b22d20af149fe66959ff0a95fd0a539209814f98
matheusbrasilaguiar <matheusbrasilaguiar@gmail.com>
2025-10-17 18:01:38 -0300

    feat: configurar UserModule com dependências

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { UserBusinessValidator } from './validators/user-business.validator';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserBusinessValidator],
   exports: [UserService],
 })
 export class UserModule {}

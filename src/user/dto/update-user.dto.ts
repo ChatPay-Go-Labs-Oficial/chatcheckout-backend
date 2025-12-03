2b8ca17e62f49818aa55b8c1fc5842a38da40139
matheusbrasilaguiar <matheusbrasilaguiar@gmail.com>
2025-12-03 20:38:41 -0300

    feat: add optional validation decorators to UpdateUserDto properties

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,28 +1,46 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEmail, IsString, IsOptional, IsEnum, MinLength } from 'class-validator';
 import { UserRole } from '../user-role.enum';
 
 export class UpdateUserDto {
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsEmail()
   email?: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  @MinLength(6)
   password?: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
   firstName?: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
   lastName?: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
   cpf?: string;
 
   @ApiPropertyOptional({ enum: UserRole })
+  @IsOptional()
+  @IsEnum(UserRole)
   role?: UserRole;
 
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
   companyName?: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
   cnpj?: string;
 }
